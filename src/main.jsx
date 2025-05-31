beca17693721ea3e3990c79c0cc441181a9cef70
sakinahathirah <sakinah.shariffudin@gmail.com>
2025-06-01 05:33:52 +0800

    1.0.2 Enhance meta tag, add code to css reset, add javascript viewport  enforcement

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,13 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>
 )
+
+const enforceViewport = () => {
+  const meta = document.querySelector('meta[name="viewport"]');
+  if (meta && window.visualViewport) {
+    meta.content = `width=${window.visualViewport.width}, initial-scale=1`;
+  }
+};
+
+window.addEventListener('resize', enforceViewport);
+window.addEventListener('load', enforceViewport);
